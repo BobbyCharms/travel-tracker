cc4842540cfd9f9cd1ce5ecfd995eae16ae2d147
Shannya <niveyro101@gmail.com>
2023-02-20 15:50:36 -0500

    first-html-update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+document.querySelector("#date-time").textContent = "Today, " + dayjs().format('dddd, MMMM D, YYYY')
+
 // DEPENDENCIES (DOM Elements)
 // DATA / STATE / GLOBAL VARIABLES
 // FUNCTIONS
