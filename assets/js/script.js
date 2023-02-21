ac760d0d31055d061fd6b470061b2739a0893d39
Laert Xhumari <laertxhumari@gmail.com>
2023-02-21 10:40:27 -0500

    fixed structure of js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-document.querySelector("#date-time").textContent = "Today, " + dayjs().format('dddd, MMMM D, YYYY')
-
 // DEPENDENCIES (DOM Elements)
+let dateTimeEl = document.querySelector("#date-time");
+
+
 // DATA / STATE / GLOBAL VARIABLES
+
+
 // FUNCTIONS
+
+
 // USER INTERACTIONS
+
+
 // INITIALIZATION
+dateTimeEl.textContent = "Today, " + dayjs().format('dddd, MMMM D, YYYY');
