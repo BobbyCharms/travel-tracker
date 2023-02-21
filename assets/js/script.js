304df509cc0a24f582496e67877a405de5e0bfe3
Laert Xhumari <laertxhumari@gmail.com>
2023-02-21 13:45:55 -0500

    the container for the map api now uses the element instead of the id name for where it's being placed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ let mapEl = document.querySelector("#map");
 // DATA / STATE / GLOBAL VARIABLES
 mapboxgl.accessToken = 'pk.eyJ1IjoibGFlcnQ5OCIsImEiOiJjbGVkNW1yM2UwMG43M3JwY2dsMjUxYjkyIn0.oODAD95bzzjfRE-Y4DhVLw';
 let map = new mapboxgl.Map({
-    container: 'map', // container ID
+    container: mapEl, // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9, // starting zoom
