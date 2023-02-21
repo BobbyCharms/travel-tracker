39fb05115cc23bc287378bc558e28b64e33b496f
Laert Xhumari <laertxhumari@gmail.com>
2023-02-21 12:22:17 -0500

    added links to the mapbox api. The map now shows up on the map element.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 // DEPENDENCIES (DOM Elements)
 let dateTimeEl = document.querySelector("#date-time");
+let mapEl = document.querySelector("#map");
 
 
 // DATA / STATE / GLOBAL VARIABLES
-
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFlcnQ5OCIsImEiOiJjbGVkNW1yM2UwMG43M3JwY2dsMjUxYjkyIn0.oODAD95bzzjfRE-Y4DhVLw';
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
 
 // FUNCTIONS
 
@@ -13,3 +20,9 @@ let dateTimeEl = document.querySelector("#date-time");
 
 // INITIALIZATION
 dateTimeEl.textContent = "Today, " + dayjs().format('dddd, MMMM D, YYYY');
+
+map.addControl(new mapboxgl.NavigationControl());
+new mapboxgl.Marker()
+.setLngLat([-74.5, 40])
+.addTo(map);
+
